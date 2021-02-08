a29a89a5444caa7e103f14e4f1a7db427e52a5bf
Frederick Lee <Frederick_Lee@wiwynn.com>
2021-02-08 16:02:44 +0800

    [sv310g4] Implement IPMI OEM system fan control commands - Manual fan speed set command:   - Netfn: 0x30, CMD: 0x11   - Request data 1: fan number 	- 0x0 to 0x5: FAN0 to FAN5 	- 0x6: all 6 fans   - Request data 2: fan speed 	- 10 to 100% - Manual/Auto fan control mode switch command:   - Netfn: 0x30 / CMD: 0x12   - Request data: 0x0 (Manual) / 0x1 (Auto) - Fan control mode get command:   - NetFn: 0x30, CMD: 0x13   - Response data: 0x0 (Manual) / 0x1 (Auto)
    Tested:
    1. Set the fan control mode to Manual and checked the status. - Pass
    root@sv310g4:~# ipmitool raw 0x30 0x12 0
    
    root@sv310g4:~# ipmitool raw 0x30 0x13
     00
    
    2. Verified the fan speed set as below.
    2.1 Set different fan speed to each fan. - Pass
    root@sv310g4:~# ipmitool raw 0x30 0x11 0 15
    
    root@sv310g4:~# ipmitool raw 0x30 0x11 1 30
    
    root@sv310g4:~# ipmitool raw 0x30 0x11 2 45
    
    root@sv310g4:~# ipmitool raw 0x30 0x11 3 60
    
    root@sv310g4:~# ipmitool raw 0x30 0x11 4 75
    
    root@sv310g4:~# ipmitool raw 0x30 0x11 5 90
    
    root@sv310g4:~# ipmitool sdr | grep "SYS FAN"
    SYS FAN0         | 3658 RPM          | ok
    SYS FAN1         | 7316 RPM          | ok
    SYS FAN2         | 11092 RPM         | ok
    SYS FAN3         | 15222 RPM         | ok
    SYS FAN4         | 19234 RPM         | ok
    SYS FAN5         | 23364 RPM         | ok
    
    2.2 Set all 6 fan speeds to 100% - Pass
    root@sv310g4:~# ipmitool raw 0x30 0x11 6 100
    
    root@sv310g4:~# ipmitool sdr | grep "SYS FAN"
    SYS FAN0         | 26078 RPM         | ok
    SYS FAN1         | 26196 RPM         | ok
    SYS FAN2         | 26904 RPM         | ok
    SYS FAN3         | 26314 RPM         | ok
    SYS FAN4         | 26550 RPM         | ok
    SYS FAN5         | 26668 RPM         | ok
    
    3. Set the fan control mode back to Auto. - Pass
    root@sv310g4:~# ipmitool raw 0x30 0x12 1
    
    root@sv310g4:~# ipmitool raw 0x30 0x13
     01

--- meta-wiwynn/meta-sv310g4/recipes-phosphor/configuration/entity-manager_git.bbappend
+++ meta-wiwynn/meta-sv310g4/recipes-phosphor/configuration/entity-manager_git.bbappend
@@ -7,6 +7,7 @@ EXTRA_OEMESON += "-Dmotherboard-config-path=/usr/share/entity-manager/configurat
 SRC_URI += "file://sv310g4-MB.json \
             file://Chicony-R800-PSU.json \
             file://blacklist.json \
+            file://fan-table.json \
             file://0001-Set-the-boot-order-of-EntityManager-after-FruDevice.patch \
             file://0002-Get-fru-device-from-motherboard-config.patch \
             file://0003-Remove-the-action-of-stopping-the-service.patch \
@@ -17,4 +18,5 @@ do_install_append(){
         install -m 0444 ${WORKDIR}/sv310g4-MB.json ${D}/usr/share/entity-manager/configurations
         install -m 0444 ${WORKDIR}/Chicony-R800-PSU.json ${D}/usr/share/entity-manager/configurations
         install -m 0444 ${WORKDIR}/blacklist.json ${D}/usr/share/entity-manager
+        install -m 0444 ${WORKDIR}/fan-table.json ${D}/usr/share/entity-manager/configurations
 }
---
+++ meta-wiwynn/meta-sv310g4/recipes-phosphor/fans/phosphor-pid-control/writePwm.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+Usage()
+{
+    echo "Usage: PWM: [-p] percentage (0 - 100) or [-v] value (0 - 255)"
+    echo "       FAN: [-z] zone id (0: for all zones, 1, 2) or [-f] fan id (1 ~ 6)"
+    exit -1
+}
+
+writePWM()
+{
+    path=$1
+    echo "$pwmValue" > "$path"
+    returnValue="$?"
+    if [ "${returnValue}" == 0 ]; then
+        if ! [ -z "$pwmPercent" ]; then
+            echo "Success write $pwmPercent% pwm to $path."
+        else
+            echo "Success write $pwmValue pwm to $path."
+        fi
+    else
+        if ! [ -z "$pwmPercent" ]; then
+            echo "Failed write $pwmPercent% pwm to $path, error code: $returnValue."
+        else
+            echo "Failed write $pwmValue pwm to $path, error code: $returnValue."
+        fi
+    fi
+}
+zoneId=0
+pwmMax=255
+while getopts "p:v:z:f:" opt; do
+    case $opt in
+        p)
+            if ! [[ "$OPTARG" =~ ^-?[0-9]+$ ]]; then
+                echo "Unknown argument. Require a number of pwm percentage after -p"
+                Usage
+            elif [ $OPTARG -gt 100 ] || [ $OPTARG -lt 0 ]; then
+                echo "Error: pwm percentage has to be between 0 - 100"
+                Usage
+            fi
+            pwmPercent=$OPTARG
+            pwmValue=$(($pwmMax * $pwmPercent / 100))
+            ;;
+        v)
+            if ! [[ "$OPTARG" =~ ^-?[0-9]+$ ]]; then
+                echo "Unknown argument. Require a number of pwm after -v"
+                Usage
+            elif [ $OPTARG -gt 255 ] || [ $OPTARG -lt 0 ]; then
+                echo "Error: pwm has to be between 0 - 255"
+                Usage
+            fi
+            pwmValue=$OPTARG
+            ;;
+        z)
+            if ! [[ "$OPTARG" =~ ^-?[0-9]+$ ]]; then
+                echo "Unknown argument. Require a zone id  after -z"
+                Usage
+            elif [ $OPTARG -ne 0 ] && [ $OPTARG -ne 1 ] && [ $OPTARG -ne 2 ]; then
+                echo "Unsupported fan id"
+                Usage
+            fi
+            zoneId=$OPTARG
+            ;;
+        f)
+            if ! [[ "$OPTARG" =~ ^-?[0-9]+$ ]]; then
+                echo "Unknown argument. Require a fan id  after -f"
+                Usage
+            elif [ $OPTARG -lt 1 ] || [ $OPTARG -gt 6 ]; then
+                echo "Unsupported zone id"
+                Usage
+            fi
+            fanIds+=($OPTARG)
+            ;;
+        *)
+            Usage
+            ;;
+    esac
+done
+
+if [ -z "$pwmValue" ]; then
+    Usage
+fi
+
+IFS=$'\n' read -rd '' -a pwmPath <<< "$(ls /sys/devices/platform/ahb/ahb:apb/1e786000.pwm-tacho-controller/hwmon/**/pwm*)"
+for path in "${pwmPath[@]}";
+do
+    pwmNum=$(echo $path | grep -Eo '[0-9]+$')
+    if ! [ -z "$fanIds" ]; then
+        for fid in "${fanIds[@]}";
+        do
+            if [ "$fid" == "$pwmNum" ]; then
+                writePWM $path
+                break
+            fi
+        done
+    else
+        if [ $zoneId -eq 1 ]; then
+            if [ $pwmNum -lt 3 ]; then
+                    wtitePWM $path
+            fi
+        elif [ $zoneId -eq 2 ]; then
+            if [ $pwmNum -gt 2 ]; then
+                    writePWM $path
+            fi
+        else
+            writePWM $path
+        fi
+    fi
+done
